3dd1ce0754c0fc8c8edb79919282ed81bb668078
your_nameAnhelina <bogdanangelina@gmail.com>
2025-08-25 14:34:42 +0300

    add

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,9 +1,26 @@
 import Accordion from 'accordion-js';
+
 new Accordion('.accordion-container', {
   duration: 400,
   triggerClass: 'ac-trigger',
   panelClass: 'ac-panel',
   activeClass: 'is-active',
-  showMultiple: false,
+  showMultiple: true,
   heightStyle: 'content',
+});
+document.querySelectorAll('.ac').forEach(ac => {
+  const trigger = ac.querySelector('.ac-trigger');
+  const panel = ac.querySelector('.ac-panel');
+
+  trigger.addEventListener('click', () => {
+    if (ac.classList.contains('is-active')) {
+      // відкриваємо
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      setTimeout(() => { panel.style.maxHeight = "none"; }, 400); // щоб після анімації висота була авто
+    } else {
+      // закриваємо
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      setTimeout(() => { panel.style.maxHeight = "0"; }, 10);
+    }
+  });
 });
